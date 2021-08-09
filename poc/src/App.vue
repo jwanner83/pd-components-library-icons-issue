582d45f9da15d4ac33ca7efd9cc7ee0ac00152df
jwa <Szjw-mudb83clnet>
2021-08-09 17:11:36 +0200

    adding correct usage of component

--- poc/src/App.vue
+++ poc/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <h1>Parlamentsdienste Components Library</h1>
+    <h1>Parlamentsdienste Components Library - Icon Issue</h1>
     <pd-icon size="3" name="parlament" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PdIcon from '@lambda-it/parlamentsdienste-component-library'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PdIcon
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, #app {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
